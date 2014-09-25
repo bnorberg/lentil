b4e120898de96f2b93378bef2ab050380ee6584e
Brian Norberg <bnorberg02@gmail.com>
2014-09-25 12:18:43 -0400

    changed setting of instagram credentials

--- lib/lentil/instagram_harvester.rb
+++ lib/lentil/instagram_harvester.rb
@@ -23,8 +23,8 @@ module Lentil
       opts['access_token'] ||= nil
 
       Instagram.configure do |config|
-        config.client_id = opts['client_id']
-        config.client_secret = opts['client_secret']
+        config.client_id = ENV['INSTAGRAM_CLIENT_ID']
+        config.client_secret = ENV['INSTAGRAM_CLIENT_SECRET']
 
         if (opts['access_token'])
           config.access_token = opts['access_token']
